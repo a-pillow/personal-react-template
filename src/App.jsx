1788deda0cc5408df9757a984bf0a7132a8e7305
dots225 <kaluhblee@gmail.com>
2022-05-03 18:56:01 -0500

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+  
+  return (
+    <h1>Hiiii - npm install and then npm run dev! </h1>
+  );
+}
+
+export default App
---
+++ src/components/ExampleComponent/ExampleComponent.jsx
@@ -0,0 +1,6 @@
+export const ExampleComponent = () => {
+
+    return (
+        <h1> you cant see me because im not being rendered!! lol</h1>
+    );
+};
